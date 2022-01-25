78e1c6739b5833381208fc905ebd2d7a9c070936
MejiaJ84 <jtmejia1984@gmail.com>
2022-01-24 18:52:32 -0800

    Configured DbContext class to use SQL Server

--- EntityFrameworkCoreExample/StudentContext.cs
+++ EntityFrameworkCoreExample/StudentContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,19 @@ namespace EntityFrameworkCoreExample
 {
     // EF core getting started 
     // https://docs.microsoft.com/en-us/ef/core/get-started/overview/install
-    internal class StudentContext : DbContext
+    public class StudentContext : DbContext
     {
+        public StudentContext()
+        {
 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // Database = the desired name of the database
+            // Server = The server we are connecting to, localDB os included with VS
+            // Trusted_Connection - indicates that our windows account should be used
+            options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EFCoreExample;Trusted_Connection=True;");
+        }
     }
 }
